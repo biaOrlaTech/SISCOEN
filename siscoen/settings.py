1347ca54deedc88f1cb5edbf4d4ebf6c91e3375c
biaOrlaTech <bia.github@orla.tech>
2025-05-09 18:05:19 -0300

    fix error files static

--- siscoen/settings.py
+++ siscoen/settings.py
@@ -182,8 +182,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    BASE_DIR / 'escala/assets',
-    BASE_DIR / 'jazzmin',
+    BASE_DIR / 'escala/templates/assets',
 ]
 
 
